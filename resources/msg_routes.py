2b9503527ecc1c7f66d705575329f2e52d0944bc
franco-giordano <francogior98@gmail.com>
2020-07-01 17:34:09 -0300

    cant send empty msg or msg to non-friends, 400 on missing token

--- resources/msg_routes.py
+++ resources/msg_routes.py
@@ -2,10 +2,11 @@
 from flask_restful import Resource, reqparse
 
 from daos.chats_dao import ChatsDAO
+from daos.users_dao import UsersDAO
 
 from services.authsender import AuthSender
 
-from exceptions.exceptions import EndpointNotImplementedError, BadRequestError, UnauthorizedError
+from exceptions.exceptions import BadRequestError, UnauthorizedError
 
 import logging
 
@@ -37,7 +38,13 @@ class MessagesRoute(Resource):
         
         args = parser.parse_args()
 
+        if args["text"] == "":
+            raise BadRequestError("Invalid message length")
+
         sender_uuid = AuthSender.get_uuid_from_token(args['x-access-token'])
+
+        if not UsersDAO.are_friends():
+            raise BadRequestError("You are not friends with this user")
 
         msg = ChatsDAO.send_message(sender_uuid, other_user_id, args["text"])
 
