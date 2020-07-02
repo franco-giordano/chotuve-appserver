6bcd5ae036c9ed8a17724b13aae34357fb0422a8
franco-giordano <francogior98@gmail.com>
2020-07-02 03:47:27 -0300

    try changing gunicorn log level to info

--- docker-entrypoint-prod.sh
+++ docker-entrypoint-prod.sh
@@ -2,4 +2,4 @@ python manage.py db init
 python manage.py db migrate
 python manage.py db upgrade
 
-gunicorn -b :$PORT run:app --log-file=- --log-level=debug
+gunicorn -b :$PORT run:app --log-file=- --log-level=info
